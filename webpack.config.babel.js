02c515689b8b243e9b4ddd10e29ffae32122c2b9
Douglas Rodrigues <douglas.rodrigues@eteg.com.br>
2017-02-26 16:01:20 -0300

    Adicionando suporte a imagens no webpack.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,47 +6,51 @@ export default {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: 'build.js',
   },
   module: {
     rules: [
-       {
+      {
         test: /\.(js|vue)$/,
         loader: 'eslint-loader',
-        enforce: "pre",
+        enforce: 'pre',
         include: [path.resolve(__dirname, './app')],
         options: {
-          formatter: require('eslint-friendly-formatter')
-        }
+          formatter: require('eslint-friendly-formatter'), // eslint-disable-line global-require
+        },
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        loader: 'style-loader!css-loader',
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        loader: 'vue-loader',
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader'
+        loader: 'url-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
       },
       {
         test: /\.svg/,
-        loader: 'svg-url-loader'
-      }
+        loader: 'svg-url-loader',
+      },
     ],
   },
   plugins: [
     new webpack.ProvidePlugin({
       jquery: 'jquery',
       $: 'jquery',
-      jQuery: 'jquery'
-    })
-  ]
+      jQuery: 'jquery',
+    }),
+  ],
 };
