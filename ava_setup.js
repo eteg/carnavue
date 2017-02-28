2ac28f04b61daa649b2a14dc5ba7c36315109163
Douglas Rodrigues <douglas.rodrigues@eteg.com.br>
2017-02-28 10:28:47 -0300

    Formatando arquivos

--- ava_setup.js
+++ ava_setup.js
@@ -1,8 +1,8 @@
 const browserEnv = require('browser-env');
 const hook = require('vue-node');
 const { join } = require('path');
- 
-// Setup a fake browser environment 
+
+// Setup a fake browser environment
 browserEnv();
-// Pass an absolute path to your webpack configuration to the hook function. 
+// Pass an absolute path to your webpack configuration to the hook function.
 hook(join(__dirname, 'webpack.config.test.js'));
